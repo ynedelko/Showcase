04c726c9412396734040d1f6a65af0863968ad66
Yelena Nedelko <ynedelko@gmail.com>
2015-08-28 16:36:27 -0700

    Clean up a bit of code.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,10 +1,23 @@
 require('spec_helper')
 
 describe('path of showcase', {:type => :feature}) do
-  it('allows a user to add a band') do
-    visit('/')
-    fill_in('name', :with => 'good band')
-    click_button('add a band')
-    expect(page).to have_content("Good band")
+  # it('allows a user to add a band') do
+  #   visit('/')
+  #   fill_in('name', :with => 'good band')
+  #   click_button('add a band')
+  #   expect(page).to have_content("Good band")
+  # end
+  # it('allows a user to add a venue') do
+  #   visit('/')
+  #   fill_in('name', :with => 'good venue')
+  #   click_button('add a venue')
+  #   expect(page).to have_content("Good venue")
+  # end
+  #need to figure out how to test for "first" fill in and "second" fill in.
+  it('lets you delete a venue') do
+    test_venue = Venue.create({:name => "Cool venue"})
+    visit("/venues/#{test_venue.id()}")
+    click_button('Delete Cool venue')
+    expect(page).to have_no_content("Cool venue")
   end
 end
