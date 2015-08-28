1c77d271c365e39c546e2742c34cc2a2cb8c173d
Yelena Nedelko <ynedelko@gmail.com>
2015-08-28 11:03:13 -0700

    Add ability to delete a band.

--- app.rb
+++ app.rb
@@ -17,3 +17,17 @@ post("/bands") do
   @bands = Band.all()
   erb(:index)
 end
+
+#going to a particular band detail page
+get('/bands/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:band_detail)
+end
+
+#deleting a band, then going back to the homepage
+delete('/bands/:id/delete') do
+  id = params.fetch("id").to_i()
+  @band = Band.find(id)
+  @band.destroy()
+  redirect("/")
+end
