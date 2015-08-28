bd2cf076fd08f77d4e00d0ce72805e756039cebe
Yelena Nedelko <ynedelko@gmail.com>
2015-08-28 12:59:36 -0700

    Allow user to add a venue, and add venue for a particular band.

--- app.rb
+++ app.rb
@@ -3,19 +3,29 @@ Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-#homepage, shows all bands
+#homepage, shows all bands and venues
 get('/') do
   @bands = Band.all()
+  @venues = Venue.all()
   erb(:index)
 end
 
-#adding a new band, added band shows up on homepage
-post("/bands") do
+#adds new band, added band shows up on homepage
+post('/bands') do
   name = params.fetch("name")
   band = Band.new({:name => name})
   band.save()
   @bands = Band.all()
-  erb(:index)
+  redirect("/")
+end
+
+#adds venue, added venue shows up on homepage
+post('/venue') do
+  name = params.fetch("name")
+  venue = Venue.new({:name => name})
+  venue.save()
+  @venues = Venue.all()
+  redirect("/")
 end
 
 #going to a particular band detail page
@@ -24,7 +34,7 @@ get('/bands/:id') do
   erb(:band_detail)
 end
 
-#updating a band, then staying band detail page to see new name
+#updating a band, then staying on band detail page to see new name
 patch('/bands/:id') do
   new_name = params.fetch("name")
   id = params.fetch("id").to_i()
@@ -39,4 +49,28 @@ delete('/bands/:id/delete') do
   @band = Band.find(id)
   @band.destroy()
   redirect("/")
+end
+
+#displays list of venues for a band
+get('/bands/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  @venue = venue.all()
+  erb(:band_detail)
+end
+
+#adds new venue to band
+post('/bands/:id/new') do
+  name = params.fetch("name")
+  band_id = params.fetch("id").to_i()
+  band = Band.find(band_id)
+  new_venue = Venue.create({:name => name})
+  band.venues.push(new_venue)
+  redirect back
+end
+
+#going to a specific venue detail page
+get('/venue/:id') do
+  @venue = Venue.find(params.fetch("id").to_i())
+  @venues = Venue.all()
+  erb(:venue_detail)
 end
